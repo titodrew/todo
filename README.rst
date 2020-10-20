c73ad1e874b93563c4bfec74649df043ce150e66
Mod <mod@core.local>
2020-10-20 04:29:49 +0200

    Initial commit - A Simple todo app.

---
+++ README.rst
@@ -0,0 +1,1 @@
+ A Simple to-do web app built with Django web framework
---
+++ todo/tasks/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render, redirect
+
+from .models import *
+from .forms import *
+
+
+def index(request):
+    tasks = Task.objects.all()
+
+    form = TaskForm()
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+
+    context = {'tasks': tasks, 'form': form}
+    return render(request, 'tasks/todo_list.html', context)
+
+
+def update_task(request, pk):
+    task = Task.objects.get(id=pk)
+
+    form = TaskForm(instance=task)
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {'form': form}
+    return render(request, 'tasks/update_task.html', context)
+
+
+def delete_task(request, pk):
+    task = Task.objects.get(id=pk)
+
+    if request.method == 'POST':
+        task.delete()
+        return redirect('/')
+
+    context = {'task': task}
+    return render(request, 'tasks/delete_task.html', context)
